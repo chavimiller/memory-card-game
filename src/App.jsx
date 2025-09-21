ca90c78ee3a14fed7a05e5345a4c44f4ab14818a
chavimiller <chaviweisman@gmail.com>
2025-09-21 15:37:01 +0300

    Add useEffect functions to fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./Card";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [cats, setCats] = useState([]);
+  const [cards, setCards] = useState([]);
+  const apiKey =
+    "live_NSctk6s9NcCNPygdBqY4cNfTfHtGa45btD1kEgDgypSOOSGUFrX8n57Ja2vh8Sv8";
+  const url = `https://api.thecatapi.com/v1/images/search?limit=50&api_key=${apiKey}`;
+
+  useEffect(() => {
+    const fetchCats = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error("Failed to fetch cat data");
+        const catData = await response.json();
+        const simplifyCats = catData.map((cat) => ({
+          id: cat.id,
+          imgUrl: cat.url,
+        }));
+        setCats(simplifyCats);
+      } catch (err) {
+        console.log("Fetch Error:", err);
+      }
+    };
+
+    fetchCats();
+  }, [url]);
+  // Test one card at index 0
+  function randomizeCats() {
+    const randomCats = [...cats].sort(() => Math.random() - 0.5).slice(0, 8);
+    setCards(randomCats);
+  }
+
+  useEffect(() => {
+    randomizeCats();
+  }, [cats]);
 
   return (
     <>
-      <Card />
+      <div className="card-grid">
+        {cards.map((card) => (
+          <Card key={card.id} cardData={card} onClick={randomizeCats} />
+        ))}
+      </div>
     </>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,18 +1,10 @@
-function Card({ cardData, onUpdate }) {
-  const apiKey = process.env.API_KEY;
-  function handleInputChange() {
-    onUpdate({
-      ...cardData,
-      [field]: value,
-    });
-  }
+function Card({ cardData, onClick }) {
   return (
     <>
       <div className="card-container">
         <div className="card-picture">
-          <img src="" alt="" />
+          <img src={cardData.imgUrl} alt="cat picture" />
         </div>
-        <div className="card-title">Name</div>
       </div>
     </>
   );
