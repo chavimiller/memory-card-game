32b3cc0c233ad01abd13a85f4504d6cbffb248d3
chavimiller <chaviweisman@gmail.com>
2025-09-21 10:33:39 +0300

    Create env file

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,20 @@
-import React from "react";
-
-function Card() {
-  return <div>Card</div>;
+function Card({ cardData, onUpdate }) {
+  function handleInputChange() {
+    onUpdate({
+      ...cardData,
+      [field]: value,
+    });
+  }
+  return (
+    <>
+      <div className="card-container">
+        <div className="card-picture">
+          <img src="" alt="" />
+        </div>
+        <div className="card-title">Name</div>
+      </div>
+    </>
+  );
 }
 
 export default Card;
